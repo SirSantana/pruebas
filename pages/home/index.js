216e4f144c6ff8003a10e089cbb4b30820fb6553
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-07-13 19:36:57 -0500

    x3

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,5 @@
 
 import { useEffect, useState } from 'react';
-import Form from '../../components/form/form';
-import PostsComponent from '../../components/posts/posts';
 import styles from '../../styles/Home.module.css'
 
 export default function Home(){
@@ -9,8 +7,6 @@ export default function Home(){
     console.log('Hola');
     return(
         <div className={styles.main}>
-            <PostsComponent />
-            <Form/>
         </div>
     )
 }
